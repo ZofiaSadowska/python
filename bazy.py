32c44d458c23971236e050b0f9a2b7b151a50eef
ZofiaSadowska <113286218+ZofiaSadowska@users.noreply.github.com>
2024-01-16 20:44:43 +0100

    Update bazy.py

--- bazy.py
+++ bazy.py
@@ -6,8 +6,24 @@ class ksiazki:
         self.data = dataWydania
         self.tytul = tytul
 
-    def tekst(self):
-        print("ksiazke o tytule", self.tytul)
+    def __str__(self):
+        return self.tytul +' '+ self.nazwisko +' '+ self.imie +' '+ self.data        
+
+def wiersz():
+    print("TYTUŁ           AUTOR            DATA")
+
+
+Id1 = ksiazki('Adam', 'Mickiewicz', 'bog wie kiedy', 'Dziady')
+Id2 = ksiazki('Henryk', 'sienkiewicz', 'nwm', 'Potop')
+
+
+
+ksiegarnia = [Id1, Id2]
+wiersz()
+for el in ksiegarnia:
+    {
+        print(el)
+    }
 
 
 
